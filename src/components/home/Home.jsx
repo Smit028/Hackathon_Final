14f6610abdf4251cf0f95e4f68cd67b7a418e8b8
Smit Tankariya <93311155+Smit028@users.noreply.github.com>
2023-08-27 12:01:11 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,6 +5,10 @@ import { Counter } from "../pages/Counter"
 import { Services } from "../pages/Services"
 import { Hero } from "./Hero"
 import Photo from "../Photo"
+import Img5 from '../data/images/Img5.png'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 import Photo2 from "../pages/Photo2"
 import Photo3 from "../pages/Photo3"
 export const Home = () => {
@@ -13,6 +17,9 @@ export const Home = () => {
       <Hero />
       <p style={{fontSize:'12px',fontWeight:'10em'}}>Upload Image Here</p>
       {/* <Photo2/> */}
+      <Row>
+        <Col><img src={Img5} height={'400px'} width={'1300px'}></img></Col>
+      </Row>
       <Photo/>
       <Photo3/>
       <About />
